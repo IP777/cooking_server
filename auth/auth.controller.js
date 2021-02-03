1d3e79e5c5bb2af190000f4bdf75ad5d0311a9b3
IP777 <ivanov_piter@ukr.net>
2021-02-03 12:54:56 +0200

    fix json req

--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -39,7 +39,7 @@ async function addUser(req, res, next) {
 
 		const newUser = await createUser.save();
 
-		res.status(200).send({ name: newUser.name, email: newUser.email });
+		res.status(200).json({ name: newUser.name, email: newUser.email });
 	} catch (err) {
 		next(err);
 	}
